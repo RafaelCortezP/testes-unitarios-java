f5e2d269fbc1b34f78046f5a9eb985b5675ccfcb
Rafael Cortez <rafaelcortezp@gmail.com>
2021-07-14 18:12:36 -0300

    Organização dos arquivos de teste

--- src/main/java/br/rafaelcortez/servicos/LocacaoService.java
+++ src/main/java/br/rafaelcortez/servicos/LocacaoService.java
@@ -4,13 +4,9 @@ import static br.rafaelcortez.utils.DataUtils.adicionarDias;
 
 import java.util.Date;
 
-import org.junit.Assert;
-import org.junit.Test;
-
 import br.rafaelcortez.entidades.Filme;
 import br.rafaelcortez.entidades.Locacao;
 import br.rafaelcortez.entidades.Usuario;
-import br.rafaelcortez.utils.DataUtils;
 
 public class LocacaoService {
 	
@@ -32,21 +28,4 @@ public class LocacaoService {
 		return locacao;
 	}
 
-	@Test
-	public void teste() {
-		
-		//cenario
-		LocacaoService service = new LocacaoService();
-		Usuario usuario = new Usuario("Rafael");
-		Filme filme = new Filme("Filme 1", 2, 5.0);
-			
-		//acao
-		 Locacao locacao = service.alugarFilme(usuario, filme);
-		
-		//verificacao
-		 Assert.assertTrue(locacao.getValor() == 5.0);
-		 Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		 Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
-		
-	}
 }
